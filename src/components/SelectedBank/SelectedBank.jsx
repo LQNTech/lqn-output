48d9f1394d4e54278da01c887ddf3cc818cf02a5
Carlos Cuatin <cecg1996@gmail.com>
2019-12-13 13:08:30 -0500

    styles

--- src/components/SelectedBank/SelectedBank.jsx
+++ src/components/SelectedBank/SelectedBank.jsx
@@ -5,6 +5,8 @@ import selectedBankQuery from './selectedBankQuery';
 
 import { resolveNodes, getBankImage } from '../../services/Helper';
 
+import { Container, SubTitle, Title } from './selectedBankStyles';
+
 const SelectedBank = () => {
   const params = new URLSearchParams(global.location.search);
 
@@ -19,9 +21,13 @@ const SelectedBank = () => {
   const selectedBank = simulators.filter(simulator => simulator.selected)[0];
 
   return (
-    <div>
+    <Container>
       <img src={getBankImage(selectedBank.bank.code)} alt="Bank" />
-    </div>
+      <div>
+        <SubTitle>Tasa de interés</SubTitle>
+        <Title>{selectedBank.nmvRate.toFixed(2)}%</Title>
+      </div>
+    </Container>
   );
 };
 
---
+++ src/components/SelectedBank/selectedBankStyles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  padding: 20px 30px;
+  border-radius: 6px;
+  border: solid 1px #f1f1f1;
+  background-color: #ffffff;
+`;
+
+export const SubTitle = styled.span``;
+
+export const Title = styled.h6``;
