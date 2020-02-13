06c3e27448d0f0c32a8e402874c79cb94c87f2bb
luisruano <lruano@altocolombia.com.co>
2020-02-13 17:19:18 -0500

    making responsive

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { Col } from 'antd';
 import { Card } from 'lqn-components';
 
 import {
@@ -38,44 +39,50 @@ const Home = ({ user, documents }) => {
         ¿Cómo quieres continuar con <Pink>tus documentos?</Pink>
       </Title>
       <Cards>
-        <Card
-          width={33.3}
-          spacing={10}
-          label="Subir documentos ahora"
-          icon={Docs}
-          onClick={() =>
-            history.push({
-              pathname: '/documents',
-              search: global.location.search
-            })
-          }
+        <Col xs={24} sm={24} md={8}>
+          <Card
+            width={33.3}
+            spacing={10}
+            label="Subir documentos ahora"
+            icon={Docs}
+            onClick={() =>
+              history.push({
+                pathname: '/documents',
+                search: global.location.search
+              })
+            }
+          />
+        </Col>
+        <Col xs={24} sm={24} md={8}>
+          <Card
+            width={33.3}
+            spacing={10}
+            label="Quiero que los recojan en mi domicilio"
+            icon={Collect}
+            onClick={() => {
+              let Calendly = global.Calendly || [];
+              user &&
+                Calendly.initPopupWidget({
+                  url: `https://calendly.com/lqn?name=${user.fullName}&email=${user.email}`
+                });
+            }}
+            footer="*Sujeto a cobertura"
         />
-        <Card
-          width={33.3}
-          spacing={10}
-          label="Quiero que los recojan en mi domicilio"
-          icon={Collect}
-          onClick={() => {
-            let Calendly = global.Calendly || [];
-            user &&
-              Calendly.initPopupWidget({
-                url: `https://calendly.com/lqn?name=${user.fullName}&email=${user.email}`
-              });
-          }}
-          footer="*Sujeto a cobertura"
-        />
-        <Card
-          width={33.3}
-          spacing={10}
-          label="Yo los llevo a LQN"
-          icon={Deliver}
-          onClick={() =>
-            history.push({
-              pathname: '/collect',
-              search: global.location.search
-            })
-          }
-        />
+        </Col>
+        <Col xs={24} sm={24} md={8}>
+          <Card
+            width={33.3}
+            spacing={10}
+            label="Yo los llevo a LQN"
+            icon={Deliver}
+            onClick={() =>
+              history.push({
+                pathname: '/collect',
+                search: global.location.search
+              })
+            }
+          />
+        </Col>
       </Cards>
     </React.Fragment>
   );
